545140cc1d73eadeef1d376ac9752360892beedd
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-10-19 17:57:06 +0300

    continue to write

--- contracts/contracts/Poster.sol
+++ contracts/contracts/Poster.sol
@@ -6,15 +6,17 @@ import "./Tickets.sol";
 
 contract Poster {
     struct Event {
-        string eventName;
         string description;
+        string URI;
         uint price;
         uint numberTickets;
+        uint totalTickets;
+        address tickets;
     }
 
     struct Organizer {
         string organizerName;
-        mapping(address => Event) events;
+        mapping(string => Event) events; //event title - Event
     }
 
     mapping(address => Organizer) public organizers;
@@ -28,22 +30,33 @@ contract Poster {
         string memory eventName,
         string memory abbreviation,
         uint price,
-        uint numberTickets
+        uint totalTickets
     ) external {
         Tickets tickets = new Tickets(eventName, abbreviation);
-        organizers[msg.sender].events[address(tickets)].eventName = eventName;
-        organizers[msg.sender].events[address(tickets)].price = price;
-        organizers[msg.sender]
-            .events[address(tickets)]
-            .numberTickets = numberTickets;
+        //address _tickets = address(tickets);
+        organizers[msg.sender].events[eventName].price = price;
+        organizers[msg.sender].events[eventName].totalTickets = totalTickets;
+        organizers[msg.sender].events[eventName].tickets = address(tickets);
     }
 
-    function buyTicket(address organizer, address events/*, uint count*/) external payable {
+    function buyTicket(
+        address organizer,
+        string memory eventName /*, uint count*/
+    ) external payable {
         require(
-            msg.value == organizers[organizer].events[events].price,
+            msg.value == organizers[organizer].events[eventName].price,
             "Need the correct amount"
         );
-        
+        require(
+            organizers[msg.sender].events[eventName].numberTickets ==
+                organizers[msg.sender].events[eventName].totalTickets,
+            "Sold out!"
+        );
+        ITicket ticket = ITicket(
+            organizers[organizer].events[eventName].tickets
+        );
+        ticket.safeMint(msg.sender, "URI");
+        organizers[msg.sender].events[eventName].numberTickets++;
     }
 
     receive() external payable {}
--- contracts/contracts/Tickets.sol
+++ contracts/contracts/Tickets.sol
@@ -3,11 +3,16 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Tickets is ERC721, ERC721Burnable, Ownable {
+interface ITicket {
+    function safeMint(address to, string memory uri) external;
+}
+
+contract Tickets is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -20,9 +25,28 @@ contract Tickets is ERC721, ERC721Burnable, Ownable {
         posterAddress = msg.sender;
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
