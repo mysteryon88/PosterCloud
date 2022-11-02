1e1643093c94eddae4814969d0e8400720666c80
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-11-02 16:53:12 +0300

    tests

--- contracts/contracts/Poster.sol
+++ contracts/contracts/Poster.sol
@@ -7,11 +7,12 @@ import "./Tickets.sol";
 contract Poster {
     struct Event {
         string description;
-        string URI;
+        string uri;
         uint price;
         uint numberTickets;
         uint totalTickets;
         address tickets;
+        uint date;
     }
 
     struct Organizer {
@@ -19,48 +20,99 @@ contract Poster {
         mapping(string => Event) events; //event title - Event
     }
 
-    string[] organizersName;
+    string[] public organizersName;
 
     mapping(address => string) public orgAddrToName; // addr - name
 
     mapping(string => Organizer) public organizers; //name - Organizer
+
+    event orgRegistered(string indexed orgName, address ordAddr);
+
+    event eventCreated(
+        string indexed orgName,
+        string indexed eventName,
+        address tickets
+    );
+
+    event bought(uint _timestamp);
 
     receive() external payable {}
 
     fallback() external payable {}
 
     function registration(string memory orgName) external payable {
-        //require(msg.value < 10000000000000000 , "It is necessary to make a contribution of 0.01 Ether");
-        //organizers[msg.sender].organizerName = name;
+        //require(msg.value < 10000000000000000 , "It is necessary to make a contribution of 0.01 Ether");ы
         organizers[orgName].organizerAddr = msg.sender;
         orgAddrToName[msg.sender] = orgName;
         organizersName.push(orgName);
+        emit orgRegistered(orgName, msg.sender);
     }
 
     function createEvent(
         string memory eventName,
         string memory abbreviation,
+        string memory uri,
+        string memory description,
         uint price,
-        uint totalTickets
-    ) external {
-        Tickets tickets = new Tickets(eventName, abbreviation);
+        uint totalTickets,
+        uint date
+    ) external onlyOrganizers {
         string memory name = orgAddrToName[msg.sender];
-        Event memory _event = organizers[name].events[eventName];
-        //address _tickets = address(tickets);
+
+        Tickets tickets = new Tickets(eventName, abbreviation);
+
+        Event memory _event;
+
         _event.price = price;
         _event.totalTickets = totalTickets;
         _event.tickets = address(tickets);
+        _event.uri = uri;
+        _event.date = date;
+        _event.description = description;
+
+        organizers[name].events[eventName] = _event;
     }
 
     function buyTicket(
         string memory organizer,
-        string memory eventName /*, uint count*/
+        string memory eventName,
+        uint count
     ) external payable {
         Event memory _event = organizers[organizer].events[eventName];
-        require(msg.value == _event.price, "Need the correct amount");
+
+        require((msg.value * count) == _event.price, "Need the correct amount");
         require(_event.numberTickets == _event.totalTickets, "Sold out!");
         ITicket ticket = ITicket(_event.tickets);
-        ticket.safeMint(msg.sender, _event.URI);
-        _event.numberTickets++;
+        //mint tickets
+        for (uint8 i = 0; i < count; ++i) {
+            ticket.safeMint(msg.sender, _event.uri);
+            _event.numberTickets++;
+        }
+        organizers[organizer].events[eventName] = _event;
+    }
+
+    function getEvent(string memory organizer, string memory eventName)
+        public
+        view
+        returns (Event memory)
+    {
+        return organizers[organizer].events[eventName];
+    }
+
+    function getTickets(string memory organizer, string memory eventName)
+        public
+        view
+        returns (address)
+    {
+        return organizers[organizer].events[eventName].tickets;
+    }
+
+    modifier onlyOrganizers() {
+        require(
+            keccak256(abi.encodePacked(orgAddrToName[msg.sender])) !=
+                keccak256(""),
+            "You are not registered as an organizer"
+        );
+        _;
     }
 }
--- contracts/test/Poster.js
+++ contracts/test/Poster.js
@@ -2,23 +2,52 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Poster", function () {
-  let owner
+  let owner, name1, name2, client1, client2, poster
 
-  beforeEach(async function () {
-    [owner] = await ethers.getSigners()
+  before(async function () {
+    [owner, name1, name2, client1, client2] = await ethers.getSigners()
     const Poster = await ethers.getContractFactory("Poster", owner)
     poster = await Poster.deploy()
-    await poster.deployed()
+
+    return { poster, name1, name2, client1, client2 }
   })
 
   it("should be deployed", async function () {
     expect(poster.address).to.be.properAddress
   })
 
-  it("check deploy", async function () {
-    let addr = await poster.addr(0)
-    console.log(addr)
-    
+  it("function registration", async function () {
+
+    await poster.connect(name1).registration("Name #1")
+    await poster.connect(name2).registration("Name #2")
+
+    expect(await poster.orgAddrToName(name1.address)).to.eq("Name #1")
+    expect(await poster.orgAddrToName(name2.address)).to.eq("Name #2")
+
+    expect(await poster.orgAddrToName(client1.address)).to.eq("")
+
+    expect(await poster.organizersName(0)).to.eq("Name #1")
+    expect(await poster.organizersName(1)).to.eq("Name #2")
+  })
+
+  it("function createEvent + modifier onlyOrganizers", async function () {
+    await poster.connect(name1).createEvent("eventName", "ENA", "uri", "description", 100, 100, 23432)
+
+    expect(await poster.organizers("Name #1")).to.eq(name1.address)
+
+    const event = await poster.getEvent("Name #1", "eventName")
+    expect(event[0]).to.eq("description")
+    expect(event[1]).to.eq("uri")
+
+    expect(event[5]).to.be.properAddress
+  })
+
+  it("modifier onlyOrganizers", async function () {
+
+    await expect(poster.connect(client1)
+      .createEvent("eventName", "ENA", "uri", "description", 100, 100, 23432))
+      .to.be.revertedWith("You are not registered as an organizer");
+      
   })
 
 });
